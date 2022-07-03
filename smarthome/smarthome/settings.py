f03b54629fafe107ce671afa0c8b21fbad992e94
Chloe <chloe.pr@pm.me>
2022-07-04 00:43:49 +0200

    update jwt token lifetime

--- smarthome/smarthome/settings.py
+++ smarthome/smarthome/settings.py
@@ -143,6 +143,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2)
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
