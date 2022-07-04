b9013cbac0d28cf0e86511e49713cea3f3f22221
Chloe <chloe.pr@pm.me>
2022-07-04 17:06:54 +0200

    make secret_key to file

--- smarthome/smarthome/settings.py
+++ smarthome/smarthome/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('/database/secret.key').read()
+try:
+    SECRET_KEY = open('/database/secret.key').read()
+except FileNotFoundError:
+    SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
