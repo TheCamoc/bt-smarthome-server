2b3437a94a3d43f9bdaf1fc96d0f4d116047a0fa
Chloe <chloe.pr@pm.me>
2022-07-09 19:09:07 +0200

    Change HyperlinkedModelSerializer to ModelSerializer

--- smarthome/backend/serializers.py
+++ smarthome/backend/serializers.py
@@ -3,34 +3,32 @@ from rest_framework import serializers
 from backend.models import Device, Switch, Room
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
 
 
-class RoomSerializer(serializers.HyperlinkedModelSerializer):
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ['url', 'name']
 
 
-class DeviceSerializer(serializers.HyperlinkedModelSerializer):
+class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
         fields = ['url', 'name']
 
 
-class SwitchSerializer(serializers.HyperlinkedModelSerializer):
-    room_name = serializers.CharField(source='room.name', read_only=True)
-
+class SwitchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Switch
-        fields = ['url', 'name', 'state', 'mqtt_topic', 'room_name']
-        read_only_fields = ['name', 'mqtt_topic', 'room_name']
+        fields = ['url', 'name', 'state', 'mqtt_topic', 'room']
+        read_only_fields = ['name', 'mqtt_topic', 'room']
