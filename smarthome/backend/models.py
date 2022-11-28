73245d98f8beb9f8e034cb09cab9df982d405201
Chloe <chloe.pr@pm.me>
2022-11-28 01:15:21 +0100

    changes in db model

--- smarthome/backend/models.py
+++ smarthome/backend/models.py
@@ -6,7 +6,7 @@ import json
 
 
 class Room(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, primary_key=True)
 
     def __str__(self):
         return self.name
